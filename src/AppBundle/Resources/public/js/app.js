6cc238c2451262d30f01f0578f2d3d4930f9333c
Flyers <contact@flyers-web.org>
2015-09-09 15:18:03 +0200

    merged @rodolfobandeira changes with path corrections

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/login', {templateUrl: '/bundles/frontend/partials/login.html', controller: 'Login'});
-    $routeProvider.when('/view1', {templateUrl: '/bundles/frontend/partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: '/bundles/frontend/partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/login', {templateUrl: '/bundles/app/partials/login.html', controller: 'Login'});
+    $routeProvider.when('/view1', {templateUrl: '/bundles/app/partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/view2', {templateUrl: '/bundles/app/partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/login'});
   }]);
