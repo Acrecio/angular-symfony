c185278082cb4f9fabcd8693f2a8f6308b75ace2
Flyers <contact@flyers-web.org>
2015-11-08 23:50:34 +0100

    Corrected missing use statement

--- src/AppBundle/Security/Firewall/WsseListener.php
+++ src/AppBundle/Security/Firewall/WsseListener.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\NonceExpiredException;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use AppBundle\Security\Authentication\Token\WsseUserToken;
