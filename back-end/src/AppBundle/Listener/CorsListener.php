16879e42a1b2199d9a7b395cf348279cbd6d29ee
Flyers <contact@flyers-web.org>
2016-06-18 23:16:41 +0200

    Allow X-WSSE

--- back-end/src/AppBundle/Listener/CorsListener.php
+++ back-end/src/AppBundle/Listener/CorsListener.php
@@ -8,7 +8,7 @@ class CorsListener
     public function onKernelResponse(FilterResponseEvent $event)
     {   
         $responseHeaders = $event->getResponse()->headers;
-        $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept, X-Requested-With');
+        $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept, X-Requested-With, X-WSSE');
         $responseHeaders->set('Access-Control-Allow-Origin', '*');
         $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
     }   
